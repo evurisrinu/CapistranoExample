a7a8baee95580245d8f34caaf411d18bd379da00
srinivasr <srinivasr@ubuntu.ubuntu-domain>
2014-04-10 12:22:26 +0530

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CapistranoExample::Application.config.secret_key_base = '7f5b571cfb96a0d1b74a7f902d4704b65a1096ef0e978e69aec9d6e76c0a57d784786140c38c5da640ccce6223f6e9d201e6b942bdd87932de35cd9205b91484'
